8c04b9ce4e829d071afbb1e6cee3ca9b4c54005d
n1vd0k <evdokov00@mail.ru>
2023-06-29 18:12:03 +0700

    SUMMER

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="App">
+        <img  src='./background1.jpg'></img>
+        <span className='App-span1'>I'm here. I'm not going anywhere. Regardless<br/> of the injury, unless it is completely <br/>debilitating, I will be the same player I have <br/>always been.</span>
+        </div>
+      
+      <div className="App">
+        <img  src='./background2.jpg'></img>
+        <span className='App-span2'>A loss is a loss. There are no different<br/> degrees of loss. Either you win the <br/>championship, or you're shit. For me it's <br/>very black and white.</span>
+        </div>
+      
+      <div className="App">
+        <img  src='./background3.jpg'></img></div>
+      
+      <div className="App">
+        <img  src='./background5.jpg'></img></div>
+      
+      <div className="App">
+        <img  src='./background4.jpg'></img>
+      </div></>
   );
 }
 
