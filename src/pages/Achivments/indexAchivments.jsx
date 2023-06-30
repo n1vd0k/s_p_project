7fdf163253335203326187020cb5adbae4a6d188
n1vd0k <evdokov00@mail.ru>
2023-06-30 15:24:27 +0700

    SUMMER

--- src/pages/Achivments/indexAchivments.jsx
+++ src/pages/Achivments/indexAchivments.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import './styleA.scss'
 const Achivments = () => {
     return(
-        <div> achivka </div>
+        <div><p className="txt">
+        5-time NBA champion : 2000, 2001, 2002, 2009, 2010<br/>
+        7 NBA Finals appearances: 2000, 2001, 2002, 2004, 2008, 2009, 2010<br/>
+        2-time NBA Finals MVP : 2009, 2010<br/>
+        NBA Most Valuable Player : 2008<br/><br/>
+        
+        2-time points champion : 2006, 2007<br/><br/>
+        
+        18-time NBA A ll-Star : 1998, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016<br/>
+        18 consecutive picks, 13 consecutive games (no All-Star game in 1999 due to a league-wide lockout)<br/>
+        missed games in 2010, 2014 and 2015 due to injury<br/>
+        
+        Four-time NBA All-Star Game MVP: 2002, 2007, 2009, 2011 (shared the 2009 award with Shaquille O'Neal)<br/><br/>
+        
+        15-time All-NBA Team selection :<br/>
+        First team : 2002, 2003, 2004, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013<br/>
+        Second team : 2000, 2001<br/>
+        Third team : 1999, 2005<br/><br/>
+        
+        12x universal defensive team selection :<br/>
+        The first team : 2000, 2003, 2004, 2006, 2007, 2008, 2009, 2010, 2011<br/>
+        Second team : 2001, 2002, 2012<br/><br/>
+        
+        NBA All-Rookie Team Selection :<br/>
+        Second team : 1997<br/><br/>
+        
+        NBA Slam Dunk Contest Champion : 1997<br/><br/>
+        
+        34-time Player of the Month : December 2000, November 2001, January 2003, March 2004, January 2006, Apr. <br/><br/>
+        
+        RIL 2006, December 2006, March 2007, April 2007, February 2008, April 2008, December 2008, January 2009,<br/><br/> 
+        
+        December 2009, March 2011, December / January 2012, February 2013<br/><br/>
+        
+        The Player of the Month was awarded separately for the Eastern and Western Conference starting with the 2001-02 season.
+        
+        </p>
+        <img className="img1" src="./site content/ac1.jpg" alt=""></img>
+        <img className="img2" src="./site content/ac2.jpg" alt=""></img>
+        </div>
     );
 }
 export default Achivments;
