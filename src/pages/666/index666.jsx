8b4e50b01dfa1afde439b42dd036402769eb5128
n1vd0k <evdokov00@mail.ru>
2023-07-03 18:56:00 +0700

    SUMMER

--- src/pages/666/index666.jsx
+++ src/pages/666/index666.jsx
@@ -1,24 +1,28 @@
-/*import React from "react";
+import React from "react";
 import "./style6.scss"
 const T666 = () => {
     return(
         <div className="body"> 
-            <div><p className="txt1">In 1996, Kobe became the first quarterback drafted by the NBA straight out of high school.<br/> Kobe was selected in the first round at number 13 by the Charlotte Hornets, but was<br/> immediately given to the Los Angeles Lakers by prior agreement of the clubs (the Lakers<br/> informed the Hornets who they should select in the draft, just a few minutes before the draft<br/> itself). 
-            </p><img className="img1" src="./site content/car1.jpg" alt=""></img>
+            <div><p className="tren-txt1">1000 ring shots and 20 minutes of dribbling.<br/>
+Kobe Bryant has a unique way of training, which is known as 666. Named so because<br/> Bryant trains 6 days a week, 6 hours a day and 6 months a year. 6 hours of training are<br/> divided into 2 hours of running, 2 hours of basketball skills and 2 hours of weightlifting <br/>(which includes one hour of cardio). 
+            </p><img className="tren-img1" src="./site content/6661.jpg" alt=""></img>
             </div>
-            <div><img className="img2" src="./site content/car2.jpg" alt=""></img>
-            <p className="txt2">Having appeared in the Lakers in a duet with the famous Shaquille O'Neal, he won three<br/> consecutive NBA champion titles (2000, 2001, 2002). After that, a feud began between them,<br/> which ended with the exchange of Shaq to the Miami Heat after the 2004 NBA Finals.<br/><br/>
-After his departure, Bryant became the main star of the Lakers and the undisputed leader and<br/> symbol of the team. Kobe began to break all possible performance records — so, in the<br/> 2005/06 and 2006/07 seasons, he was recognized as the best sniper of the regular season.<br/><br/>
-In 2006, he scored 81 points in a game with the Toronto Raptors, setting the second record for<br/> points scored in one game, after Wilt Chamberlain's one-point match in 1962.<br/><br/> 
-In 2008, he was named the most valuable player of the NBA. The Lakers, led by Bryant, lost the<br/> 2008 NBA Finals, but in the 2008/09 and 2009/10 seasons, Kobe led the Lakers to an NBA<br/> victory twice more. Kobe was named the most valuable player of both finals. </p>
+            <div><img className="tren-img2" src="./site content/6662.jpg" alt=""></img>
+            <p className="tren-txt2">
+            Basketball exercises:<br/>
+Basketball training includes throws around the ring at least 700-1000 times and 20 minutes of<br/> dribbling. Bryant also performs at least 10 minutes of defensive drills, followed by an intense<br/> 90 minutes of throws. He throws from five specific areas of the court, making 10 throws from<br/> each point before moving on to the next. Before throwing from the three-point line, he usually <br/>starts from the middle distance. He also works on throws with deviations.
+            </p>
             </div>
             <div>
-                <p className="txt3">At the age of 34 years 104 days, Bryant became the youngest player in the history of<br/> the league to break the 30,000 point milestone. He also became the most productive<br/> Lakers player in history.<br/><br/>
-On November 29, 2015, he announced that this was the last season of his career<br/><br/>
-In the game against the Utah Jazz — his last official NBA match — Bryant scored 60<br/> points, becoming the oldest player to whom this mark was submitted. Kobe also<br/> became the first in the history of the league to score so many points in a farewell<br/> match.</p>
-            <img className="img3" src="./site content/car3.jpg"></img>
+                <p className="tren-txt3">
+                Kobe Bryant's Workout Routine:<br/>
+Kobe has a crazy training method that is very intense. During the off-season, Kobe<br/> mainly focuses on training in order to increase strength and endurance. As the season<br/> approaches, he begins to increase the conditional aspect of training.<br/> He trains six days a week, because he believes that the muscles need to be<br/> given a day to rest and recover. He usually performs the exercise for 8-12<br/> repetitions, which is the best method to achieve muscle hypertrophy<br/> 
+Kobe uses acupuncture techniques to prevent injury.<br/>
+According to Michael Jordan, Kobe is the only basketball player who can compare with him.</p>
+            <img className="tren-img3" src="./site content/6663.jpg"></img>
+            <p></p>
             </div>
         </div>
     );
 }
-export default T666;*/
+export default T666;
