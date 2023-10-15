58226805ef3fb8c443cacc015b0fe308d2df9df7
n1vd0k <evdokov00@mail.ru>
2023-10-16 00:32:12 +0700

    last

--- src/pages/Home/indexHome.jsx
+++ src/pages/Home/indexHome.jsx
@@ -5,20 +5,23 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <><div className="Home">
+              <span className='Home-span1'>I'm here. I'm not going anywhere. Regardless of the injury, unless it is completely debilitating, I will be the same player I have <br/>always been.</span>
+
         <img  src='./background1.jpg' alt=""></img>
-        <span className='Home-span1'>I'm here. I'm not going anywhere. Regardless<br/> of the injury, unless it is completely <br/>debilitating, I will be the same player I have <br/>always been.</span>
         </div>
       
       <div className="Home">
         <h1 className="Home-h1">Careere</h1>
-      <Link to ={'/Careere'}><img  src='./background2.jpg'alt=""></img></Link>
         <span className='Home-span2'>A loss is a loss. There are no different<br/> degrees of loss. Either you win the <br/>championship, or you're shit. For me it's <br/>very black and white.</span>
+
+      <Link to ={'/Careere'}><img  src='./background2.jpg'alt=""></img></Link>
         </div>
       
       <div className="Home">
       <h1 className="Home-h2">Achivments</h1>
-      <Link to ={'/Achivments'}><img  src='./background3.jpg'alt=""></img></Link>
       <span className='Home-span3'>When you understand what failure is, determination pursues success.</span>
+
+      <Link to ={'/Achivments'}><img  src='./background3.jpg'alt=""></img></Link>
       </div>
       
       <div className="Home">
